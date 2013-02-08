fdeab2d58dc84fc45fdb46762aa44283494c87f1
carchrae <tom@carchrae.net>
2013-02-07 16:02:48 -0800

    simplified version that does not try and parse versions.

--- install-to-project-repo.py
+++ install-to-project-repo.py
@@ -6,8 +6,9 @@
 # 
 # MIT License
 # (c) 2012, Nikita Volkov. All rights reserved.
-# http://github.com/nikita-volkov/install-to-project-repo
-# 
+# simplified version adapted by Tom Carchrae
+# https://github.com/carchrae/install-to-project-repo
+
 
 
 import os
@@ -20,25 +21,15 @@ def jars(dir):
 
 def parse_by_eclipse_standard(path):
   file = os.path.splitext(os.path.basename(path))[0]
-  match = re.match("([\w\.]+)_(\d+\.\d+\.\d+.*)", file)
-  if match != None:
-    (name, version) = match.group(1, 2)
 
-    name = name.split(".")
-    source = name[-1] == "source"
-    (group, name) = (".".join(name[:-2]), name[-2]) if source else (".".join(name[:-1]), name[-1])
+  return {
+  "group": "local",
+	"name": file,
+	"version": "1.0",
+	"snapshot": "",
+	"source": ""
+  }
 
-    snapshot = version.upper().endswith(".SNAPSHOT")
-    if snapshot:
-      version = version[:-len(".snapshot")]
-
-    return {
-      "group": group,
-      "name": name,
-      "version": version,
-      "snapshot": snapshot,
-      "source": source
-    }
 
 def maven_dependencies(parsing_results):
   def artifact(parsing):
@@ -79,127 +70,9 @@ def install(path, parsing):
     (" -Dclassifier=sources" if parsing["source"] else "")
   )
 
-
-def splits(str, splitter):
-  parts = str.split(splitter)
-  def split(i):
-    (l, r) = splitAt(parts, i) 
-    return (splitter.join(l), splitter.join(r))
-
-  return map(split, range(1, len(parts)))
-
-def splitAt(list, i):
-  if i <= 0:
-    return ([], list)
-  elif i >= len(list):
-    return (list, [])
-  else:
-    return (list[:i], list[i:])
-
-def name_to_version_alternatives(filename):
-  return [
-    (n, v) 
-    for (n, v) in splits(filename, "-") + splits(filename, "_")
-    if v.lower() not in ["sources", "src", "snapshot"]
-  ]
-
-def group_to_name_alternatives(group):
-  return [
-    (n, v)
-    for (n, v) in splits(group, ".")
-    if (v.lower()) not in ["source", "snapshot"]
-  ]
-
-def version_parsing(version):
-
-  def f(version, splitter, ending):
-    parts = version.split(splitter)
-    if parts[-1] == ending:
-      return (splitter.join(parts[:-1]), True)
-    else:
-      return (version, False)
-
-  (version, source) = f(version, "-", "sources")
-  (version, snapshot) = f(version, "-", "SNAPSHOT")
-  if not snapshot:
-    (version, snapshot) = f(version, ".", "SNAPSHOT")
-
-  return version, snapshot, source
-
-def name_parsing(name):
-  if name.endswith(".source"):
-    return name[:-len(".source")], True
-  else:
-    return name, False
-
-def unzip(l): 
-  return tuple(zip(*l))
-
-def input_choice(labels, values):
-  for (i, v) in enumerate(labels):
-    print "%d) %s" % (i+1, v)
-  while True:
-    try:
-      i = raw_input()
-      i = int(i)
-      return values[i-1]
-    except ValueError:
-      print "Incorrect input: `%s` is not a number. Try again" % i
-    except IndexError:
-      print "Incorrect input: `%s` is out of range. Try again" % i
-
-
-def parse_interactively(path):
-  filename = os.path.splitext(os.path.basename(path))[0]
-
-  print "-----"
-  print "Processing `%s`" % path
-
-  alternatives = name_to_version_alternatives(filename)
-  alternatives.sort(key=lambda (n, v): len(v), reverse=True)
-
-  if not alternatives:
-    print "Incorrect name format: `%s`. Skipping" % filename
-    return
-  if len(alternatives) > 1:
-    print "Choose a correct version for `%s`:" % filename
-    labels = [version_parsing(v)[0] for v in unzip(alternatives)[1]]
-    (name, version) = input_choice(labels, alternatives)
-  else:
-    (name, version) = alternatives[0]
-
-  
-  alternatives = list(reversed(group_to_name_alternatives(name)))
-  if not alternatives:
-    print "Incorrect name format: `%s`. Skipping" % filename
-    return
-  if len(alternatives) > 1:
-    print "Choose a correct artifactId for `%s`:" % name
-    labels = [name_parsing(a)[0] for a in unzip(alternatives)[1]]
-    (group, name) = input_choice(labels, alternatives)
-  else:
-    (group, name) = alternatives[0]
-
-
-  version, snapshot, source = version_parsing(version)
-  name, source1 = name_parsing(name)
-
-  return {
-    "group": group,
-    "name": name,
-    "version": version,
-    "snapshot": snapshot,
-    "source": source or source1
-  }
-
-
-
 from optparse import OptionParser
 
 parser = OptionParser()
-parser.add_option("-i", "--interactive", 
-                  dest="interactive", action="store_true", default=False,
-                  help="Interactively resolve ambiguous names. Use this option to install libraries of different naming standards")
 parser.add_option("-d", "--delete", 
                   dest="delete", action="store_true", default=False, 
                   help="Delete successfully installed libs in source location")
@@ -207,8 +80,6 @@ parser.add_option("-d", "--delete",
 
 
 parsings = (
-  [(path, parse_interactively(path)) for path in jars("lib")]
-  if options.interactive else
   [(path, parse_by_eclipse_standard(path)) for path in jars("lib")]
 )
 
@@ -227,3 +98,6 @@ for (path, parsing) in parsings:
     os.remove(path)
 
 print maven_dependencies(parsings)
+print "These dependenceis have also been saved in the file : repo/dependencies.txt"
+f = open('repo/dependencies.txt', 'w')
+f.write(maven_dependencies(parsings))
